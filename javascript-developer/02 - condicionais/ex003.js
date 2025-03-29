ebe00093906b8974c16d5d60eba845d9ec91c997
Felipe <90115621+felipesabbado@users.noreply.github.com>
2025-03-29 19:25:32 +0000

    reorganizando o código

---
+++ javascript-developer/02 - condicionais/ex003.js
@@ -0,0 +1,25 @@
+/* Faça um algoritmo que, dado as 3 notas tiradas por um aluno em um semestre
+ * da faculdade, calcule e imprima a sua média e a sua classificação conforme
+ * conforme a tabela abaixo.
+ * 
+ * Média = (nota 1 + nota 2 + nota 3) / 3;
+ * 
+ * Classificação:
+ * - Média menor que 5, reprovado;
+ * - Média entre 5 e 7, em recuperação;
+ * - Média acima de 7, aprovado no semestre.
+ */
+
+const nota1 = 7.5;
+const nota2 = 8.3;
+const nota3 = 6.4;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+if (media < 5) {
+    console.log(`Média ${media.toFixed(1)} - REPROVADO`);
+} else if (media < 7) {
+    console.log(`Média ${media.toFixed(1)} - RECUPERAÇÃO`);
+} else {
+    console.log(`Média ${media.toFixed(1)} - APROVADO`);
+}
