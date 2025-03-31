014dc11013d1d680e5d2e40abd05110eb9684910
Felipe <90115621+felipesabbado@users.noreply.github.com>
2025-03-31 13:26:31 +0100

    arrays

---
+++ javascript-developer/05 - arrays/aula001.js
@@ -0,0 +1,15 @@
+// Declara uma lista (array)
+const alunos = ['João', 'Vitor', 'Marina'];
+
+// Adicionar elementos à lista
+alunos.push('Renan');
+alunos[4] = 'Vinícius';
+
+console.log(alunos)
+
+// Remover elementos da lista
+console.log('Removendo o último aluno: ' + alunos.pop())
+console.log('Removendo o primeiro aluno: ' + alunos.shift)
+
+console.log(alunos)
+// console.log(alunos[0])
